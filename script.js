adb89c11348d56cc9795dc9333771f9460f86baa
hanaho fujikawa <hanagreenms06@gmail.com>
2020-08-16 00:33:12 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function cssTest() {
+    const body = document.getElementById("body");
+    const cover = document.getElementById("cover");
+    const inner = document.getElementById("inner")
+    body.animate({
+        opacity: [0, 1]
+        }, {
+        direction: 'reverse',
+        duration: 800,
+    })
+    cover.animate({
+        opacity: [0, 1]
+        }, {
+        direction: 'reverse',
+        duration: 800,
+    }).onfinish = changeTitle;
+}
+
+function changeTitle() {
+
+    document.getElementById("body").classList.toggle("neon");
+    document.getElementById("body").classList.toggle("colorful");
+
+    if (document.getElementById("body").classList.contains("neon")) {
+        document.getElementById("title").innerHTML = "Neon";
+    }
+    else {
+        document.getElementById("title").innerHTML = "Light";
+    }
+    inner.animate({
+        opacity: [0, 1]
+        }, 1000);
+}
+
